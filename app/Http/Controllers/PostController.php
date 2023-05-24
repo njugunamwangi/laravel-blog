954ff4c546b8f5063bbce524456d46984292ac14
njugunamwangi <njugunamwangi.n@gmail.com>
2023-05-24 19:47:32 +0300

    get post by category

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -68,27 +69,7 @@ class PostController extends Controller
         return view('post.view', compact('post', 'prev', 'next'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
+    public function byCategory(Category $category) {
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
-        //
     }
 }
