7f7d581484e8bb99cc1b1fbf7700c588ada0d394
njugunamwangi <njugunamwangi.n@gmail.com>
2023-05-26 18:49:44 +0300

    create admin user on backend

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,27 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use Carbon\Carbon;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array {
+        $data['email_verified_at'] = Carbon::now();
+
+        return $data;
+    }
+
+    protected function handleRecordCreation(array $data) : Model {
+
+        /** @var \App\Models\User $user */
+        $user = parent::handleRecordCreation($data);
+        $user->assignRole('admin');
+
+        return $user;
+    }
 }
