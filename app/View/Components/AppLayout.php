a2bbd33ae51c9958da0f796b709b741ec5814697
njugunamwangi <njugunamwangi.n@gmail.com>
2023-05-26 12:40:24 +0300

    install breeze user management

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -5,17 +5,25 @@ namespace App\View\Components;
 use App\Models\Category;
 use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\Component;
 
 class AppLayout extends Component
 {
+    // public Collection $categories;
     /**
      * Create a new component instance.
      */
     public function __construct(public ?string $metaTitle = null, public ?string $metaDescription = null)
     {
-
+        $categories = Category::query()
+                                ->join('category_post', 'categories.id', '=', 'category_post.category_id')
+                                ->select('categories.title', 'categories.slug', DB::raw('count(*) as total'))
+                                ->groupBy('categories.id')
+                                ->orderByDesc('total')
+                                ->limit(5)
+                                ->get();
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
     <meta name="description" content="{{ $metaDescription }}">
 
     <!-- Tailwind -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
 
@@ -20,10 +19,10 @@
         }
     </style>
 
-    <!-- AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-white font-family-karla">
 
