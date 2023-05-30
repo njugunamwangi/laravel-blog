88f1dda8658dcdfb45baf4e463af768582e2820d
njugunamwangi <njugunamwangi.n@gmail.com>
2023-05-30 15:15:39 +0300

    db seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        /** @var \App\Models\User $adminUser */
         // \App\Models\User::factory(10)->create();
+        // $adminUser = User::factory()->create([
+        //     'email' => 'admin@ndachi.dev',
+        //     'name' => 'Admin',
+        //     'password' => bcrypt('admin123')
+        // ]);
 
+        // $adminRole = Role::create(['name' => 'admin']);
+        // $adminUser->assignRole($adminRole);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
