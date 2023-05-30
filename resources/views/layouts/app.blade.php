78cb34477f26177c3ecc42a71fd14d223ea1a7ac
njugunamwangi <njugunamwangi.n@gmail.com>
2023-05-30 15:15:19 +0300

    Add livewire to frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -17,6 +16,7 @@
 
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+    @livewireStyles
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -116,6 +116,6 @@
             <div class="uppercase pb-6">&copy; myblog.com</div>
         </div>
     </footer>
-
+    @livewireScripts
 </body>
 </html>
